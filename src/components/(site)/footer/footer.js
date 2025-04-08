e33b3fd29059c68d8a27f15c3f8533f88c1f6af3
Mike Boldsen <mikeboldsen2004@gmail.com>
2025-04-08 18:54:18 +0200

    footer 99%

--- src/components/(site)/footer/footer.js
+++ src/components/(site)/footer/footer.js
@@ -28,60 +28,61 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       
-      
-      <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
 
-            {footerData?.map( (footerItem) => {
-              return ( 
-              <div key={footerItem.id} className={styles.footerGroup}>
-                {footerItem.subTitle ? <div className={styles.headingContainer}><h3 className={styles.footerHeading}>{footerItem?.name}</h3><p>{footerItem.subTitle}</p></div> : <h3 className={styles.footerHeading}>{footerItem?.name}</h3>}
-                
-                {footerItem?.menus ? (
-                  <div className={styles.menus}>
+              {footerData?.map( (footerItem) => {
+                return ( 
+                <div key={footerItem.id} className={styles.footerGroup}>
+                  {footerItem.subTitle ? <div className={styles.headingContainer}><h3 className={styles.footerHeading}>{footerItem?.name}</h3><p>{footerItem.subTitle}</p></div> : <h3 className={styles.footerHeading}>{footerItem?.name}</h3>}
+                  
+                  {footerItem?.menus ? (
+                    <div className={styles.menus}>
 
-                    {footerItem?.menus?.map( (menu, index) => {
-                      return (
-                        <Link key={index} href={menu?.link}>{menu?.name}</Link>
-                      );
-                    })}
-
-                  </div> 
-                ) : footerItem?.description ? (
-                <div className={styles.footerIntro}>
-                  <div className={styles.description}>
-                    {footerItem?.description}
-                  </div>
-                  {footerItem?.socials ? <div className={styles.footerSocials}>
-                      {footerItem?.socials?.map( (social, index) => {
+                      {footerItem?.menus?.map( (menu, index) => {
                         return (
-                        <Link key={index} href={social.link} target='_blank'>
-                           {social.icon}
-                      </Link> )})}
-                      
-                
-                    </div> : ""}
-                </div>
-            ) : footerItem?.contactInfo ? (
-                <div className={styles.footerContact}>
-                  <div className={styles.contactInfo}>
-                    {footerItem?.contactInfo?.map( (info, index) => {
-
-                      return (
-                        <div className={styles.contact} key={index}>
-                          <div className={styles.icon}>{info.icon}</div>
-                          <div>{info?.mail ? (<Link href={`mailto:${info.mail}`} target='_blank'>{info.text}</Link>) : info.phone ? (<Link href={`tel:+${info.phone}`}>{info.text}</Link>) : ""}</div>
-                        </div> 
+                          <Link key={index} href={menu?.link}>{menu?.name}</Link>
                         );
                       })}
-                    
-                  </div>
-                </div>
-                ) : ""}
-              
-              </div>
-              );
-            })}
 
+                    </div> 
+                  ) : footerItem?.description ? (
+                  <div className={styles.footerIntro}>
+                    <div className={styles.description}>
+                      {footerItem?.description}
+                    </div>
+                    {footerItem?.socials ? <div className={styles.footerSocials}>
+                        {footerItem?.socials?.map( (social, index) => {
+                          return (
+                          <Link key={index} href={social.link} target='_blank'>
+                            {social.icon}
+                        </Link> )})}
+                        
+                  
+                      </div> : ""}
+                  </div>
+              ) : footerItem?.contactInfo ? (
+                  <div className={styles.footerContact}>
+                    <div className={styles.contactInfo}>
+                      {footerItem?.contactInfo?.map( (info, index) => {
+
+                        return (
+                          <div className={styles.contact} key={index}>
+                            <div className={styles.icon}>{info.icon}</div>
+                            <div>{info?.mail ? (<Link href={`mailto:${info.mail}`} target='_blank'>{info.text}</Link>) : info.phone ? (<Link href={`tel:+${info.phone}`}>{info.text}</Link>) : ""}</div>
+                          </div> 
+                          );
+                        })}
+                      
+                    </div>
+                  </div>
+                  ) : ""}
+                
+                </div>
+                );
+              })}
+
+        </div>
       </div>
 
       <div className={styles.footerBottom}>
