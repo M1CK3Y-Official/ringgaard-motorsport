f00b115ea212310ee423e39f624c375d6468b96c
Mike Boldsen <mikeboldsen2004@gmail.com>
2025-04-20 02:57:39 +0200

    Added  "Kontakt" page

--- src/app/(site)/kontakt/page.js
+++ src/app/(site)/kontakt/page.js
@@ -1,27 +1,74 @@
+import Label from '@/components/(site)/label/label';
 import styles from './page.module.css';
 import Car from '@/components/(site)/car/car';
+import SubHero from '@/components/(site)/subhero/subhero';
 import Image from "next/image"
 import Link from 'next/link'
 
 export default function Page() {
 
+    const subheroConfig = {
+        title: 'Kontakt',
+        subtitle: 'Kontakt',
+        image: '/Heros/Om-os.jpg'
+    }
+
     return (
 
-        <div className={styles.container}> 
-            <div className={styles.hero}>
+        <>
+        
+            <SubHero config={subheroConfig} />
 
-                <Image src='/Heros/Bilen.jpg' alt="hero" width={4246} height={2830}></Image>
+            <div className='wrapper'>
 
-                <div className={styles.overlay}></div>
+                <div className={'textContainer'}>
+                    <Label>HENVENDELSER</Label>
+                    <h2>Kontakt os</h2>
+                    <div className='body'>
+                        <p>
+                            Hvis du har spørgsmål til mig eller teamet omkring et eller andet, eller hvis du gerne vil lave et samarbejde med os, så tøv ikke med at kontakte os! 
+                        </p>
 
-                <div className={styles.header}>
-                    <h1>Om Bilerne</h1>
-                    <p>Legend Car</p>
+                        <form action={''}>
+                            <div className={styles.formContainer}>
+                                <label htmlFor="name">Navn:</label>
+                                <input type="text" id="name" name="name" required />
+
+                                <label htmlFor="email">E-mail:</label>
+                                <input type="email" id="email" name="email" required />
+
+                                <label htmlFor="message">Besked:</label>
+                                <textarea className={styles.message} id="message" name="message" rows="4" required></textarea>
+
+                                <button type="submit">Send</button>
+                            </div>
+                        </form>
+
+                        <p >
+                            Vi kan kontaktes på:
+                        </p>
+                        
+                        <p style={{ display: "flex", flexDirection: "column" }}>
+
+                            <span>
+                                Tlf: 30 32 47 51
+                            </span>
+                            <span>
+                                Mail: m.ringgaard@icloud.com
+                            </span>
+                        </p>
+
+                        <p>
+                            Eller på facebooksiden Mathias Ringgaard Motorsport, hvor du altid kan skrive til os på Messenger.
+                        </p>
+
+                        
+                    </div>
                 </div>
 
             </div>
-            <Car></Car>
-        </div>
+        
+        </>
 
     )
 }
