c3a4470130410f91abad9be5673c9295d26f9f7c
Mike Boldsen <mikeboldsen2004@gmail.com>
2025-09-28 20:32:37 +0200

    Progress on Team component

--- src/components/(site)/team/team.js
+++ src/components/(site)/team/team.js
@@ -5,11 +5,7 @@ const Team = () => {
   return (
     <section className={styles.team}>
       Team
-
-      <div className="buttonGroup">
-        <Link href={'/'} className='button'>Gå videre til bilen</Link>
-        <Link href={'/'} className='button'>Om os siden</Link>
-      </div>
+      
     </section>
   )
 };
